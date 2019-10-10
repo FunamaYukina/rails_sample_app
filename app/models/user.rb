4468ba3e66427d4a3494e73d99683288be1836a5
FunamaYukina <be-with-you1228@i.softbank.jp>
2019-10-10 16:07:19 +0900

    13.1.2 fix error

--- app/models/user.rb
+++ app/models/user.rb
@@ -55,9 +55,9 @@ class User < ApplicationRecord
   # パスワード再設定の属性を設定する
   def create_reset_digest
     self.reset_token = User.new_token
-    # update_attribute(:reset_digest,  User.digest(reset_token))
-    # update_attribute(:reset_sent_at, Time.zone.now)
-    update_columns(reset_digest:  FILL_IN, reset_sent_at: FILL_IN)
+    update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+    # update_columns(reset_digest:  FILL_IN, reset_sent_at: FILL_IN)
 
   end
 
