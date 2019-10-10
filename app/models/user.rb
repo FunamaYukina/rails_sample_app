3bbcbd24fe5c4cf3448280570addcf3990bea95b
FunamaYukina <be-with-you1228@i.softbank.jp>
2019-10-10 10:10:06 +0900

    11.1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
+  before_save   :downcase_email
+  before_create :create_activation_digest
+
 
   before_save { self.email = email.downcase }
   validates :name,  presence: true, length: { maximum: 50 }
@@ -37,6 +40,19 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, nil)
   end
 
+
+  private
+
+  # メールアドレスをすべて小文字にする
+  def downcase_email
+    self.email = email.downcase
+  end
+
+  # 有効化トークンとダイジェストを作成および代入する
+  def create_activation_digest
+    self.activation_token  = User.new_token
+    self.activation_digest = User.digest(activation_token)
+  end
 
 end
 
