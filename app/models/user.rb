20f78271d2a8c9a6cb6871c9bab79fac56e6b6d0
FunamaYukina <be-with-you1228@i.softbank.jp>
2019-10-11 09:34:40 +0900

    13.3

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,18 @@
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
-  attr_accessor :remember_token, :activation_token,:reset_token
-  before_save   :downcase_email
+  attr_accessor :remember_token, :activation_token, :reset_token
+  before_save :downcase_email
   before_create :create_activation_digest
 
 
   before_save { self.email = email.downcase }
-  validates :name,  presence: true, length: { maximum: 50 }
+  validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255 },
-            format: { with: VALID_EMAIL_REGEX },
-            uniqueness: { case_sensitive: false }
+  validates :email, presence: true, length: {maximum: 255},
+            format: {with: VALID_EMAIL_REGEX},
+            uniqueness: {case_sensitive: false}
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+  validates :password, presence: true, length: {minimum: 6}, allow_nil: true
 
   # 渡された文字列のハッシュ値を返す
   def User.digest(string)
@@ -20,6 +20,7 @@ class User < ApplicationRecord
                BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
+
   # ランダムなトークンを返す
   def User.new_token
     SecureRandom.urlsafe_base64
@@ -28,15 +29,16 @@ class User < ApplicationRecord
   # 永続セッションのためにユーザーをデータベースに記憶する
   def remember
     self.remember_token = User.new_token
-    update_attribute(:remember_digest,User.digest(remember_token))
+    update_attribute(:remember_digest, User.digest(remember_token))
   end
 
   # 渡されたトークンがダイジェストと一致したらtrueを返す
-  def authenticated?(attribute,token)
+  def authenticated?(attribute, token)
     digest = self.send("#{attribute}_digest")
     return false if digest.nil?
     BCrypt::Password.new(digest).is_password?(token)
   end
+
   # ユーザーのログイン情報を破棄する
   def forget
     update_attribute(:remember_digest, nil)
@@ -44,7 +46,7 @@ class User < ApplicationRecord
 
   # アカウントを有効にする
   def activate
-    update_attribute(:activated,    true)
+    update_attribute(:activated, true)
     update_attribute(:activated_at, Time.zone.now)
   end
 
@@ -56,7 +58,7 @@ class User < ApplicationRecord
   # パスワード再設定の属性を設定する
   def create_reset_digest
     self.reset_token = User.new_token
-    update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_digest, User.digest(reset_token))
     update_attribute(:reset_sent_at, Time.zone.now)
     # update_columns(reset_digest:  FILL_IN, reset_sent_at: FILL_IN)
 
@@ -72,6 +74,13 @@ class User < ApplicationRecord
     reset_sent_at < 2.hours.ago
   end
 
+  # 試作feedの定義
+  # 完全な実装は次章の「ユーザーをフォローする」を参照
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
+
+
   private
 
   # メールアドレスをすべて小文字にする
@@ -81,7 +90,7 @@ class User < ApplicationRecord
 
   # 有効化トークンとダイジェストを作成および代入する
   def create_activation_digest
-    self.activation_token  = User.new_token
+    self.activation_token = User.new_token
     self.activation_digest = User.digest(activation_token)
   end
 
